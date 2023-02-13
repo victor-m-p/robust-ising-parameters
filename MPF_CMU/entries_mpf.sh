6ea073711a554cfe0bdc4645a5b7aa0600751771
victor-m-p <victormoeller@gmail.com>
2023-02-13 10:58:06 -0500

    ran some of the n=10

--- MPF_CMU/entries_mpf.sh
+++ MPF_CMU/entries_mpf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-FILES="../data/sample_questions/mdl/*"
+FILES="../data/sample_questions/mdl_10/*"
 
 for f in $FILES
 do
-    nohup ./mpf -c "$f" > "question_fit_5.txt"
+    nohup ./mpf -c "$f" > "question_fit_10x.txt"
 done 

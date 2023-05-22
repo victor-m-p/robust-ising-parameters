a30538d75d10e8a0fd2b63ecef87caaceca170fc
victor-m-p <victormoeller@gmail.com>
2023-05-22 10:29:52 -0400

    new data for l1 norm

--- MPF_CMU/sim_fully_connected_grid.sh
+++ MPF_CMU/sim_fully_connected_grid.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-dir="../simulation/data/fully_connected_nn20_nsim500_mpf"
+dir="../simulation/data/fully_connected_nn13_nsim500_l1_mpf"
 generate_random_string() {
     cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1
 }
@@ -18,7 +18,7 @@ do
         do
             random_id=$(generate_random_string)
             output_file="${file}_${grid_value}_${random_id}_log.txt"
-            ./mpf -l "$file" "$grid_value" 2.0 > "$output_file" &
+            ./mpf -l "$file" "$grid_value" 1.0 > "$output_file" &
             # If the number of background jobs is equal to the number of cores, wait for them to finish
             if (( i % $(nproc) == 0 )); then
                 wait
